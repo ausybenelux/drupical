bf728ef666dc2493658883fc267641534d417129
Bart Arickx <bart.arickx@crosscheck.be>
2015-02-27 13:52:29 +0100

    php5 settings

--- build/vagrant/chef-repo/cookbooks-custom/php5/attributes/default.rb
+++ build/vagrant/chef-repo/cookbooks-custom/php5/attributes/default.rb
@@ -8,4 +8,6 @@ default["php5"]["key_ondrej_old"] = 'E5267A6C'
 
 default["php5"]["version"] = '5.5'
 
+default["php5"]['php_fpm_pool']["php_options"] = {'php_admin_flag[log_errors]' => 'on', 'php_admin_value[memory_limit]' => '512M', 'php_admin_value[error_reporting]' =>  'E_ALL & ~E_DEPRECATED', 'php_admin_value[display_errors]'  =>  'On', 'php_admin_value[post_max_size]'  =>  '64M', 'php_admin_value[upload_max_filesize]' =>  '64M'}
+
 
--- build/vagrant/chef-repo/cookbooks-custom/php5/recipes/install.rb
+++ build/vagrant/chef-repo/cookbooks-custom/php5/recipes/install.rb
@@ -32,7 +32,7 @@ include_recipe 'php'
 php_fpm_pool "www" do
   process_manager "dynamic"
   max_requests 5000
-  php_options 'php_admin_flag[log_errors]' => 'on', 'php_admin_value[memory_limit]' => '512M', 'php_admin_value[error_reporting]' =>  'E_ALL & ~E_DEPRECATED', 'php_admin_value[display_errors]'  =>  'On', 'php_admin_value[post_max_size]'  =>  '64M', 'php_admin_value[upload_max_filesize]' =>  '64M'
+  php_options default["php5"]['php_fpm_pool']["php_options"]
 end
 
 file "/etc/apache2/conf-available/php-fpm.conf" do
@@ -44,7 +44,6 @@ template '/etc/apache2/conf-available/php-fpm.conf' do
   source 'php-fpm.conf'
   mode '0644'
   notifies :restart, 'service[apache2]', :delayed
-  only_if {!File.exists?('/etc/apache2/conf-available/php-fpm.conf')}
 end
 
 link '/etc/apache2/conf-enabled/php-fpm.conf' do
