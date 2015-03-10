187a8bcbca943ee1eceab91ebb3385222588a690
Bart Arickx <bart.arickx@crosscheck.be>
2015-03-10 23:01:41 +0100

    fix selenium & chrome

--- build/vagrant/chef-repo/cookbooks-custom/testing-selenium/recipes/default.rb
+++ build/vagrant/chef-repo/cookbooks-custom/testing-selenium/recipes/default.rb
@@ -12,9 +12,24 @@ selenium_node 'selenium_node' do
       browserName: 'chrome',
       maxInstances: 5,
       seleniumProtocol: 'WebDriver'
+    },
+    {
+      browserName: 'firefox',
+      maxInstances: 5,
+      seleniumProtocol: 'WebDriver'
     }
   ]
   action :install
+end
+
+file "/etc/init.d/selenium_node" do
+  action :delete
+  only_if { File.exists?("/etc/init.d/selenium_node") }
+end
+
+template "/etc/init.d/selenium_node" do
+  source "selenium_node"
+  mode 0777
 end
 
 include_recipe 'testing-selenium::xvfb'
--- docroot/WebTest.php
+++ docroot/WebTest.php
@@ -1,22 +1,41 @@
 <?php
-
-class WebTest extends PHPUnit_Extensions_SeleniumTestCase
+class WebTest extends PHPUnit_Extensions_Selenium2TestCase
 {
-
-    protected $captureScreenshotOnFailure = TRUE;
-    protected $screenshotPath = '/home/vagrant/drupical/docroot';
-    protected $screenshotUrl = 'http://localhost';
-
-    protected function setUp()
-    {
-        $this->setBrowser('chrome');
-        $this->setBrowserUrl('http://www.example.com/');
-        $this->setPort(5555);
+    /**
+     * Variable to specify which browsers to run the tests on
+     * @var array
+     */
+    public static $browsers = [
+        ['browserName' => 'firefox'],
+        ['browserName' => 'chrome'] 
+    ];
+ 
+    public function __construct() {
+        $this->setHost('localhost');
+        $this->setPort(4444);
+        $this->setDesiredCapabilities([]);
     }
-
-    public function testTitle()
+ 
+    public function setUp()
     {
-        $this->open('http://www.example.com/');
-        $this->assertTitle('Example WWW Page');
+        $this->setBrowserUrl('http://www.example.com/');
+    }
+ 
+    /**
+     * This is just a test that will open a website in chrome and firefox
+     */
+    public function testOpenSite()
+    {
+
+
+        $this->url('/');
+
+        $fp = fopen('/home/vagrant/drupical/docroot/screenshot.' . time() . '.png','wb');
+
+        fwrite($fp, $this->currentScreenshot());
+
+        fclose($fp);
+                        
+        $this->assertEquals('Example WWW Page', $this->title());        
     }
 }
