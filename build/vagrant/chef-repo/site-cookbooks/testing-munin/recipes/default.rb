a757541d02116a24472a6d394d6a73d3f8d940b4
Bart Arickx <bart.arickx@crosscheck.be>
2015-03-12 18:07:53 +0100

    fix template info & installation munin

--- build/vagrant/chef-repo/site-cookbooks/testing-munin/recipes/default.rb
+++ build/vagrant/chef-repo/site-cookbooks/testing-munin/recipes/default.rb
@@ -1,45 +1,12 @@
 #
-# Cookbook Name:: testing-munin
+# Cookbook Name::testing-munin
 # Recipe::munin
 #
 
 if node['config']['web_tools']['tools']['munin']['install']
 
-	package "munin" do
-		action :install
-	end
-
-	template "/etc/munin/munin-conf.d/munin-folders" do
-		source "munin-folders"
-		mode 0777
-	end
-
-	git "/opt/munin-monitoring" do
-		repository "https://github.com/munin-monitoring/contrib.git"
-		action :sync
-		notifies :run, "execute[move-templates]", :immediately
-	end
-
-	execute "move-templates" do
-		command <<-EOH
-		  (mv /etc/munin/templates /etc/munin/factory)
-		  (mkdir /etc/munin/templates)
-		  (mkdir /etc/munin/templates/munstrap)
-		  (mv /etc/munin/factory /etc/munin/templates)
-		  (cp -avr /opt/munin-monitoring/templates/munstrap/templates/. /etc/munin/templates/munstrap)
-		  (cp -avr /opt/munin-monitoring/templates/munstrap/static/. /etc/munin/templates/munstrap)
-		EOH
- 		action :nothing
-	end
-
-	url_base = node['config']['web_tools']['url_base']
-	tool_alias = node['config']['web_tools']['tools']['munin']['alias']
-
-	web_app "munin" do
-		server_name "#{tool_alias}.#{url_base}"
-		docroot "/var/cache/munin/www"
-		cookbook 'apache2'
-		allow_override 'All'
-	end
+  include_recipe 'testing-munin::munin_repo'
+  
+  include_recipe 'testing-munin::munin_install'
 
 end
---
+++ build/vagrant/chef-repo/site-cookbooks/testing-munin/recipes/plugins.rb
@@ -0,0 +1,4 @@
+#
+# Cookbook Name::testing-munin
+# Recipe::plugins
+#
