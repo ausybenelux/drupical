f9a13eaece8522782b68d4c5ff8689c11ce5f081
Sebastiaan Provost <sebastiaan.provost@crosscheck.be>
2015-03-13 17:11:43 +0100

    SSL Support should have been added

--- vagrant/chef-repo/site-cookbooks/web/recipes/apache.rb
+++ vagrant/chef-repo/site-cookbooks/web/recipes/apache.rb
@@ -41,7 +41,30 @@ execute "Enabling necessary apache2 modules" do
 end
 
 #
- node['config']['vhosts'].each do |key, vhost|
+
+directory "/etc/apache2/ssl" do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+node['config']['vhosts'].each do |key, vhost|
+
+  enable_ssl = false
+  vhost_port = 80
+  if vhost['enable_ssl'] == 'true'
+    enable_ssl = true
+    vhost_port = 443
+   openssl_x509 "/etc/apache2/ssl/#{vhost['server_name']}.crt" do
+     common_name "#{vhost['server_name']}"
+     org "One Agency"
+     org_unit "Web"
+     country "BE"
+   end
+  end
+
+
 
   web_app key do
     templates 'web_app.conf.erb'
@@ -50,6 +73,8 @@ end
     server_aliases vhost['aliases']
     docroot vhost['docroot']
     allow_override 'All'
+    enable_ssl enable_ssl
+    server_port vhost_port
     server_pool vhost['server_name'].split('.')[0]
   end
 
